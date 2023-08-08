465dd2b24d6310e01984559811624dfcbef842c3
amritanandsingh <amritanandsingh999@gmail.com>
2023-08-08 12:13:24 +0530

    main.cpp is ready for development along with header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,39 @@
 #include <iostream>
 #include "../header/typeFactory.hpp"
-
+#include "../header/authentication.hpp"
+#include<string>
+using namespace std;
 int main() {
     
     
     
-
+    string genral_user , userid , password;
     
-    user* user1 = Factory::createVehicle("manager");
-    user1->whoIm(); // Output: "I am manager"
+    cout<<"who you are ? ";
+    cin>>genral_user;
 
-    user* user2 = Factory::createVehicle("customer");
-    user2->whoIm(); // Output: "I am customer"
+    user* user = Factory::createVehicle(genral_user);
+    string type = user->whoIm(); // Output: type of class
+    
+    if(user==nullptr)
+    {
+        cout<<"invalid user";
+    }
+    else{
+        cout<<endl<<"Enter userID and Password "<<endl;
+        cin>>userid>>password;
+        authentication auth_obj;
+        if(auth_obj.logIn(userid,password,type))
+        {
+            cout<<"sucessfull log in ";
 
-    delete user1;
-    delete user2;
+        }
+        else{
+            cout<<"Invalid userid or password ";
+        }
+    }
+
+    delete user;
 
     return 0;
 }
