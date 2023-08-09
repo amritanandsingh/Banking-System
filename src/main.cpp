3fd4791115ae19b9ec8141b2c4215bcb5066c0c4
amritanandsingh <amritanandsingh999@gmail.com>
2023-08-09 11:43:14 +0530

    add new class and fix some bug and customer_function_provider  class ready to development

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,57 @@
 #include <iostream>
 #include "../header/typeFactory.hpp"
 #include "../header/authentication.hpp"
+#include "../header/signUp.hpp"
 #include<string>
+//#include<stdexcept>
 using namespace std;
 int main() {
     
     
-    
-    string genral_user , userid , password;
-    
-    cout<<"who you are ? ";
-    cin>>genral_user;
-
-    user* user = Factory::createVehicle(genral_user);
-    string type = user->whoIm(); // Output: type of class
-    
-    if(user==nullptr)
-    {
-        cout<<"invalid user";
-    }
-    else{
-        cout<<endl<<"Enter userID and Password "<<endl;
-        cin>>userid>>password;
-        authentication auth_obj;
-        if(auth_obj.logIn(userid,password,type))
+    try{
+        string genral_user , userid , password ,type;
+        
+        cout<<"who you are ? if New type 'signup' for Sign Up "<<endl;
+        cin>>genral_user;
+        Factory factory_obj;
+        if(genral_user=="signup")
         {
-            cout<<"sucessfull log in ";
+            SignUp signUp_obj;
+            signUp_obj.form();
+            cout<<"sign Up successfully"<<endl;
+            return 0;
+        }
+        User *user= factory_obj.createUser(genral_user);
 
+        if(user!=NULL)
+        {
+            type = user->whoIm(); // Output: type of class
+        }
+        if(user==NULL)
+        {
+            cout<<"Invalid User Type"<<endl;
+            exit(0);
         }
         else{
-            cout<<"Invalid userid or password ";
+            cout<<endl<<"Enter userID and Password "<<endl;
+            cin>>userid>>password;
+            authentication auth_obj;
+            if(auth_obj.logIn(userid,password,type))
+            {
+                cout<<"sucessfull log in "<<endl;
+                user->menu(userid);
+            }
+            else{
+                cout<<"Invalid userid or password "<<endl;
+            }
         }
+        delete user;
     }
-
-    delete user;
-
+    catch(...)
+    {
+        std::cerr << "An error occurred: "<< std::endl;
+    }
+    
+    
     return 0;
 }
